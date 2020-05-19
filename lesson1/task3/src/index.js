2556626ca4e9462fa0b70aec35377a031844cb9c
Milkweedd <antontab91@gmail.com>
2020-05-20 02:29:05 +0300

    sa

--- lesson1/task3/src/index.js
+++ lesson1/task3/src/index.js
@@ -1,18 +1,14 @@
-
 import React from "react";
-import ReactDom from "react-dom";
+import ReactDOM from "react-dom";
 import "./styles.css";
-
-
-
 
 const rootElement = document.querySelector("#root");
 
-const elem = (
+const element = (
   <div className="greeting">
     <div className="greeting__title">Hello, World!</div>
     <div className="greeting__text">I'm learning React</div>
   </div>
 );
 
-ReactDom.render(elem, rootElement);
+ReactDOM.render(element, rootElement);

b531415cd5193af06fefe5b36409bf3e6de9ed29
Milkweedd <antontab91@gmail.com>
2020-05-21 20:22:32 +0300

    565

---
+++ lesson3/task1/src/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDom from "react-dom";
+import "./styles.css";
+import "./styles.scss";
+
+
+const rootElement = document.querySelector('#root');
+
+const Greeting = () => {
+
+};
+
+const element = (
+  <div className="search">
+
+  </div>
+);
+
+ReactDom.render(element, rootElement);

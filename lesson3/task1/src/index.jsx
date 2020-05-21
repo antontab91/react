7ae99b715759bc3fcdcf90f8e8c416a960f42ebd
Milkweedd <antontab91@gmail.com>
2020-05-21 21:05:26 +0300

    as

--- lesson3/task1/src/index.jsx
+++ lesson3/task1/src/index.jsx
@@ -12,7 +12,11 @@ const Greeting = () => {
 
 const element = (
   <div className="search">
-
+    <h1 className="search__title">Hello, Tom. What to search for you?</h1>
+    <div className="search__field">
+      <input type="text" className="search__input" />
+      <button className="search__button">Search</button>
+    </div>
   </div>
 );
 

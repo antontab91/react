a627c03a6cc1790931a25b14dadbdf165d076e44
Milkweedd <antontab91@gmail.com>
2020-05-21 21:40:54 +0300

    as

--- lesson3/task1/src/Search.jsx
+++ lesson3/task1/src/Search.jsx
@@ -4,10 +4,10 @@ import "./search.scss";
 
 
 
-export const Search = (name) => {
+export default (props) => {
   return (
     <div className="search">
-      <h1 className="search__title">{`Hello, ${name.name}. What to search for you?`}</h1>
+      <h1 className="search__title">{`Hello, ${props.name}. What to search for you?`}</h1>
       <div className="search__field">
         <input type="text" className="search__input" />
         <button className="search__button">Search</button>

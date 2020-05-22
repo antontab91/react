139e70af634ebd1244e7ca0bc4e9076e7d296968
Milkweedd <antontab91@gmail.com>
2020-05-22 04:35:19 +0300

    as

--- lesson3/task4/src/Greeting.jsx
+++ lesson3/task4/src/Greeting.jsx
@@ -7,7 +7,7 @@ const actualData = (data) => {
 const Greeting = (props) => {
   return (
     <div className="greeting">
-      {`My name is ${props.firstName} Doe. I'm 17 years old`}
+      {`My name is ${props.firstName} ${props.lastName}. I'm 17 years old`}
     </div>
   )
 }

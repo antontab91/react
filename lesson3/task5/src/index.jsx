80a0fd71f81f24196b5f3dadcfdb2888ac5e5c29
Milkweedd <antontab91@gmail.com>
2020-05-22 13:29:32 +0300

    as

---
+++ lesson3/task5/src/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const userData = {
+  firstName: 'John',
+  lastName: 'Doe',
+  birthDate: '1991-01-17T11:11:11.819Z',
+  birthPlace: 'London'
+};
+
+

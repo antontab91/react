3e3f2f58f2400de622e36233bfe740da7744253b
Milkweedd <antontab91@gmail.com>
2020-05-27 00:22:34 +0300

    dsa

---
+++ lesson4/task1/src/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDom from "react-dom";
+import "./index.scss";
+
+const rootElement = document.querySelector("#root");
+
+const element = <div></div>
+
+
+ReactDom.render(element, rootElement);

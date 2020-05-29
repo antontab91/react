ca246e2f8d5e0d1c0379f5ac6fd0277542978693
Milkweedd <antontab91@gmail.com>
2020-05-29 10:13:02 +0300

    sasa

---
+++ lesson6/task5/src/Logout.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Logout = ({onLogout}) => {
+    return (
+        <button className="btn logout" 
+        onClick={onLogout}>
+            Logout
+        </button>
+    );
+};
+
+export default Logout;
+

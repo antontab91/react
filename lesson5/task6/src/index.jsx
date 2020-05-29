886e1697f276213157bdc1bcf0c37b071c27d67d
Milkweedd <antontab91@gmail.com>
2020-05-29 10:15:00 +0300

    ds

--- lesson5/task6/src/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ColorPicker from './ColorPicker';
-import './index.scss';
-
-const rootElem = document.querySelector('#root');
-
-ReactDOM.render(<ColorPicker />, rootElem);

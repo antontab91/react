76e84d45eaeb8cc2c509c94be58c155bdbf77c97
Milkweedd <antontab91@gmail.com>
2020-06-04 01:34:29 +0300

    as

--- lesson8/task4/src/User.jsx
+++ lesson8/task4/src/User.jsx
@@ -1,33 +1,33 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 class User extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             user: null,
         }
     }
 
-    fetchUser = userId => {
-        fetch(`https://api.github.com/users/${userId}`)
-            .then(response => response.json())
-            .then(data => 
-                this.setState({
-                    user: data,
-                }));
-    };
+    // fetchUser = userId => {
+    //     fetch(`https://api.github.com/users/${userId}`)
+    //         .then(response => response.json())
+    //         .then(data => 
+    //             this.setState({
+    //                 user: data,
+    //             }));
+    // };
 
     componentDidMount() {
         this.fetchUser(this.props.userId);
     }
 
     render() {
-        const {user} = this.state;
+        const { user } = this.state;
 
-        if(!user)
+        if (!user)
             return null;
 
-        const {avatar_url, name, location} = user;
+        const { avatar_url, name, location } = user;
         return (
             <div className="user">
                 <img

a350b6587b0c82dd46dde432ccb871b3f08ab076
Milkweedd <antontab91@gmail.com>
2020-06-15 09:49:30 +0300

    sa

--- lesson4/task2/src/Clock.jsx
+++ lesson4/task2/src/Clock.jsx
@@ -1,2 +1,14 @@
+import React, { Component } from 'react';
 
 
+
+class Clock extends Component {
+  constructor(props) {
+    super(props);
+
+
+
+  }
+}
+
+export default Clock;
--- lesson4/task2/src/index.jsx
+++ lesson4/task2/src/index.jsx
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 
+
+
+// 3
+// 4
+// 5
+// 6
+// 7
+// 8
+// 9
+
 const rootElem = document.querySelector('#root');
 
 

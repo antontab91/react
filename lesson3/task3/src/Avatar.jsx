16f3b2b94d4ecbf12e61188f166fdbcfce4a664d
Milkweedd <antontab91@gmail.com>
2020-06-17 23:12:36 +0300

    as

--- lesson3/task3/src/Avatar.jsx
+++ lesson3/task3/src/Avatar.jsx
@@ -4,8 +4,8 @@ const Avatar = (props) => {
   return (
     <img
       className="avatar"
-      src={props.user.avatarUrl}
-      alt={props.user.name}
+      src={props.avatarUrl}
+      alt={props.name}
     />
   );
 };

04442c8789192b6bb65a62be9fe1b7ce89de8161
Milkweedd <antontab91@gmail.com>
2020-06-17 14:04:54 +0300

    sa

--- lesson3/task2/src/index.jsx
+++ lesson3/task2/src/index.jsx
@@ -1,12 +1,8 @@
-import React from "react";
-import ReactDom from "react-dom";
-import "./styles.css";
-import Search from "./Search.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
+import Search from './Search.jsx';
 
+const root = document.querySelector('#root');
 
-
-const rootElement = document.querySelector('#root');
-
-
-
-ReactDom.render(<Search name="Antony" />, rootElement);
+ReactDOM.render(< Search name="Tom" />, root);

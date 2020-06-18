92dae99e06d1936780af5c55941144ba3e2317d0
Milkweedd <antontab91@gmail.com>
2020-06-19 01:03:20 +0300

    as

--- lesson5/task1/src/index.jsx
+++ lesson5/task1/src/index.jsx
@@ -1,8 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import GoodButton from './GoodButton';
-import './index.scss';
-
-const rootElem = document.querySelector('#root');
-
-ReactDOM.render(<GoodButton />, rootElem);
+import React from

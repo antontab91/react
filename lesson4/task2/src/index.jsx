c6c884138e50711e3bbf4a0e2afcdba623293574
Milkweedd <antontab91@gmail.com>
2020-06-18 17:39:13 +0300

    ыф

--- lesson4/task2/src/index.jsx
+++ lesson4/task2/src/index.jsx
@@ -1,19 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
 
-
-
-// 3
-// 4
-// 5
-// 6
-// 7
-// 8
-// 9
-
-const rootElem = document.querySelector('#root');
-
-
-ReactDOM.render(<App />, rootElem);

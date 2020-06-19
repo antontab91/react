6f609342d05198f449816e9a3f908f8361b53848
Milkweedd <antontab91@gmail.com>
2020-06-19 23:39:03 +0300

    ds

--- lesson5/task3/src/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Counter from './Counter';
-import './index.scss';
-
-const rootElem = document.querySelector('#root');
-
-ReactDOM.render(<Counter />, rootElem);

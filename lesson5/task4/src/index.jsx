7f67371abbea84b85059ab19a0cf252055ed80c1
Milkweedd <antontab91@gmail.com>
2020-06-20 01:30:03 +0300

    as

--- lesson5/task4/src/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Colors from './Colors';
-import './index.scss';
-
-const rootElem = document.querySelector('#root');
-
-ReactDOM.render(<Colors />, rootElem);

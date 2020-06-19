e7dde7ac9775499ad1a10e6929541c4c409cdc7f
Milkweedd <antontab91@gmail.com>
2020-06-20 01:26:47 +0300

    sa

--- lesson5/task3/src/Counter.jsx
+++ lesson5/task3/src/Counter.jsx
@@ -1,77 +1,41 @@
-// import React from 'react';
+import React from 'react';
 
-// class Counter extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             counter: 0,
-//         }
-//     }
-
-//     decrease() {
-//         this.setState({
-//             counter: this.state.counter - 1,
-//         });
-//     }
-
-//     increase = () => {   //это эксперементальная запись
-//         this.setState({
-//             counter: this.state.counter + 1,
-//         })
-//     }
-
-//     reset() {
-//         this.setState({
-//             counter: 0,
-//         })
-//     }
-
-
-//     render() {
-//         return (
-//             <div className="counter">
-//                 <button data-action="decrease" onClick={this.decrease.bind(this)} className="counter__button">-</button>
-//                 <span className="counter__value" onClick={() => { return this.reset() }}> {this.state.counter}</span>
-//                 <button data-action="increase" onClick={this.increase} className="counter__button">+</button>
-//             </div>
-//         );
-//     }
-// }
-// export default Counter;
-
-import React, { Component } from "react";
-
-class Counter extends Component {
+class Counter extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             counter: 0,
-        };
-        this.decrement = this.decrement.bind(this);
+        }
+        this.decrease = this.decrease.bind(this);
     }
-    decrement() {
+
+    decrease() {
         this.setState({
             counter: this.state.counter - 1,
-        })
+        });
     }
-    increment = () => {
+
+    increase = () => {   //это эксперементальная запись
         this.setState({
             counter: this.state.counter + 1,
         })
     }
+
     reset() {
         this.setState({
             counter: 0,
         })
     }
+
+
     render() {
         return (
             <div className="counter">
-                <button className="counter__button" onClick={this.decrement}>-</button>
-                <span className="counter__value" onClick={() => this.reset()}>{this.state.counter}</span>
-                <button className="counter__button" onClick={this.increment}>+</button>
-            </div>)
+                <button data-action="decrease" onClick={this.decrease} className="counter__button">-</button>
+                <span className="counter__value" onClick={() => { return this.reset() }}> {this.state.counter}</span>
+                <button data-action="increase" onClick={this.increase} className="counter__button">+</button>
+            </div>
+        );
     }
 }
-
 export default Counter;

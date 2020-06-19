defec711639646d4e3aef453bb11abd234661b3e
Milkweedd <antontab91@gmail.com>
2020-06-20 02:23:13 +0300

    ds

--- lesson5/task5/src/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Toggler from './Toggler';
-import './index.scss';
-
-const rootElem = document.querySelector('#root');
-
-ReactDOM.render(<Toggler />, rootElem);

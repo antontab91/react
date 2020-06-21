ddc1e49c4833c7a982d41fa996c042738e119c4a
Milkweedd <antontab91@gmail.com>
2020-06-22 01:12:41 +0300

    fd

---
+++ lesson5/task5/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Toggler from './Toggler.jsx';
+
+const App = () => {
+  return (
+    // <div className="sa">0</div>
+    <Toggler />
+  )
+}
+
+export default App;
---
+++ lesson5/task5/src/Toggler.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Toggler extends React.Component {
+  render() {
+    return (
+      <div className="as">as</div>
+    )
+  }
+}
+
+export default Toggler;

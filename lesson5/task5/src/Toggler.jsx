32d5d62ed15c35abc5ea38d4d40439df403efc6f
Milkweedd <antontab91@gmail.com>
2020-06-22 02:09:37 +0300

    sd

--- lesson5/task5/src/Toggler.jsx
+++ lesson5/task5/src/Toggler.jsx
@@ -1,47 +1,54 @@
-// import React from 'react';
+import React from 'react';
 
 // const offText = 'Off';
 // const onText = 'On';
 
-// class Toggler extends React.Component {
+class Toggler extends React.Component {
 
-//   setOnfOff(event) {
-//     event.target.textContent === offText
-//       ? event.target.textContent = onText
-//       : event.target.textContent = offText;
-//   }
-
-//   render() {
-//     return (
-//       <div onClick={this.setOnfOff.bind(this)} className="toggler">{offText}</div>
-//     )
-//   }
-// }
-
-// export default Toggler;
-
-
-import React, { Component } from "react";
-
-class Toggle extends Component {
   constructor(props) {
-    super(props);
-    this.state = {
-      isToggleOn: false
-    };
+    super(props)
+    this.state{
+
+    }
   }
-  handleClick = () => {
+
+  setOnfOff(event) {
     this.setState({
-      isToggleOn: !this.state.isToggleOn
-    });
-  };
+
+    })
+  }
+
   render() {
     return (
-      <div onClick={this.handleClick} className="toggler">
-        {this.state.isToggleOn ? "On" : "Off"}
-      </div>
-    );
+      <div onClick={this.setOnfOff.bind(this)} className="toggler">{offText}</div>
+    )
   }
 }
 
-export default Toggle;
+export default Toggler;
+
+
+// import React, { Component } from "react";
+
+// class Toggle extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       isToggleOn: false
+//     };
+//   }
+//   handleClick = () => {
+//     this.setState({
+//       isToggleOn: !this.state.isToggleOn
+//     });
+//   };
+//   render() {
+//     return (
+//       <div onClick={this.handleClick} className="toggler">
+//         {this.state.isToggleOn ? "On" : "Off"}
+//       </div>
+//     );
+//   }
+// }
+
+// export default Toggle;

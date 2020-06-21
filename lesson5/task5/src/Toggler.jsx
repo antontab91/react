9604097acf90e05c85cf969d598cccdbde82ae1a
Milkweedd <antontab91@gmail.com>
2020-06-22 01:59:14 +0300

    sa

--- lesson5/task5/src/Toggler.jsx
+++ lesson5/task5/src/Toggler.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
+const offText = 'Off';
+const onText = 'On';
+
 class Toggler extends React.Component {
+
+  setOnfOff(event) {
+    event.target.textContent === offText
+      ? event.target.textContent = onText
+      : event.target.textContent = offText;
+  }
+
   render() {
     return (
-      <div className="as">as</div>
+      <button onClick={this.setOnfOff.bind(this)} className="toggler">{offText}</button>
     )
   }
 }

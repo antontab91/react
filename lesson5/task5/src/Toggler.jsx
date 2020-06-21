3d527ea7e014fbd1fb062a48dc26ac7733403dca
Milkweedd <antontab91@gmail.com>
2020-06-22 02:02:33 +0300

    ds

--- lesson5/task5/src/Toggler.jsx
+++ lesson5/task5/src/Toggler.jsx
@@ -13,9 +13,35 @@ class Toggler extends React.Component {
 
   render() {
     return (
-      <button onClick={this.setOnfOff.bind(this)} className="toggler">{offText}</button>
+      <div onClick={this.setOnfOff.bind(this)} className="toggler">{offText}</div>
     )
   }
 }
 
 export default Toggler;
+
+
+// import React, { Component } from "react";
+
+// class Toggle extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       isToggleOn: false
+//     };
+//   }
+//   handleClick = () => {
+//     this.setState({
+//       isToggleOn: !this.state.isToggleOn
+//     });
+//   };
+//   render() {
+//     return (
+//       <div onClick={this.handleClick} className="toggler">
+//         {this.state.isToggleOn ? "On" : "Off"}
+//       </div>
+//     );
+//   }
+// }
+
+// export default Toggle;

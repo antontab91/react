d94e31ce7978aff209cecb55470efc254f31fb29
Milkweedd <antontab91@gmail.com>
2020-06-22 23:58:39 +0300

    as

---
+++ lesson5/task6/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ColorPicker from './ColorPicker';
+
+const App = () => {
+  return (
+    <ColorPicker />
+  );
+}
+
+export default App;
---
+++ lesson5/task6/src/ColorPicker.jsx
@@ -0,0 +1,48 @@
+const CORAL = "coral";
+const AQUA = "aqua";
+const BISQUE = "bisque";
+
+import React from 'react';
+
+class ColorPicker extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'dsa'
+    }
+  }
+
+  showNameOfColor(event) {
+    this.setState({
+      title: event.target.style.backgroundColor
+    })
+  }
+
+  resetNameofColor() {
+    this.setState({
+      title: ''
+    })
+  }
+
+  render() {
+    return (
+      <>
+        <div>
+          <div className="picker__title">
+            {this.state.title}
+          </div>
+          <div>
+            <button className="picker__button picker__button_coral" onMouseOver={this.showNameOfColor.bind(this)} onMouseOut={this.resetNameofColor.bind(this)} style={{ background: CORAL }}></button>
+            <button className="picker__button picker__button_aqua" onMouseOver={this.showNameOfColor.bind(this)} onMouseOut={this.resetNameofColor.bind(this)} style={{ background: AQUA }}></button>
+            <button className="picker__button picker__button_bisque" onMouseOver={this.showNameOfColor.bind(this)} onMouseOut={this.resetNameofColor.bind(this)} style={{ background: BISQUE }}></button>
+          </div>
+        </div>
+
+
+      </>
+    )
+  }
+}
+
+export default ColorPicker;

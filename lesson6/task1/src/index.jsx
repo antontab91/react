663908143b26ac0eabff8659f355f10eeb17bb43
Milkweedd <antontab91@gmail.com>
2020-06-23 02:14:34 +0300

    s

--- lesson6/task1/src/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from'react';
-import ReactDOM from 'react-dom';
-import Auth from './Auth';
-import './index.scss';
-
-const rootElem = document.querySelector('#root');
-
-ReactDOM.render(<Auth />, rootElem);

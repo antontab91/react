8542251f72ca947a2dabb2eeeb9ad6408be87010
Milkweedd <antontab91@gmail.com>
2020-06-23 02:47:16 +0300

    as

---
+++ lesson6/task1/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Auth from './Auth.jsx'
+
+const App = () => {
+  return (
+    <Auth />
+  )
+}
+
+export default App;
---
+++ lesson6/task1/src/Auth.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import Greeting from './Greeting';
+import Logout from './Logout';
+import Login from './Login';
+
+class Auth extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isLoggedIn: false,
+        };
+    }
+
+    handleLogIn = () => {
+        this.setState({
+            isLoggedIn: true,
+        });
+    }
+
+    handleLogOut = () => {
+        this.setState({
+            isLoggedIn: false,
+        })
+    }
+
+    render() {
+        return (
+            <div className="panel">
+                <Greeting isLoggedIn={this.state.isLoggedIn} />
+                <div>
+                    {(!this.state.isLoggedIn) ?
+                        <Login onLogin={this.handleLogIn} /> :
+                        <Logout onLogout={this.handleLogOut} />
+                    }
+                </div>
+            </div>
+        );
+    }
+}
+export default Auth;

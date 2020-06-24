bbefea91b0297b41ec49ccdc7076ecfe2b626f28
Milkweedd <antontab91@gmail.com>
2020-06-25 01:34:38 +0300

    sa

--- lesson6/task4/src/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Status from './Status';
-import './index.scss';
-
-const rootElem = document.querySelector('#root');
-
-ReactDOM.render(<Status />, rootElem);

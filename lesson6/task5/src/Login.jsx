5bd299e543a19069e772034a533f4becf6987b95
Milkweedd <antontab91@gmail.com>
2020-06-26 00:30:06 +0300

    sd

---
+++ lesson6/task5/src/Login.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Login = ({ onLogin }) => {
+    return (
+        <button class="login btn">Login</button>
+    )
+}
+
+export default Login;
---
+++ lesson6/task5/src/Logout.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Logout = ({ onLogout }) => {
+    return (
+        <button class="logout btn">Logout</button>
+    )
+}
+
+export default Logout;

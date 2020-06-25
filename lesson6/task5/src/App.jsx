41ee8f7eca312f6086d0a3641639f9f6177ac900
Milkweedd <antontab91@gmail.com>
2020-06-26 00:49:09 +0300

    as

---
+++ lesson6/task5/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Spinner from './Spinner.jsx';
+
+const App = () => {
+  return (
+    <div>sds</div>
+  )
+}
+
+export default App;
--- lesson6/task5/src/Spinner.jsx
+++ lesson6/task5/src/Spinner.jsx
@@ -1,1 +1,14 @@
 import React from 'react';
+
+const Spinner = ({ size }) => {
+
+    const style = {
+        width: `${size}`,
+    }
+
+    return (
+        <span style={{ style }} class="spinner" ></span >
+    )
+}
+
+export default Spinner;

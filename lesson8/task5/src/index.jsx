a0709678bad8e8530cdc221b3a55639dbd19bc76
Milkweedd <antontab91@gmail.com>
2020-06-26 23:26:43 +0300

    DS

--- lesson8/task5/src/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ConnectionStatus from './ConnectionStatus';
-import './index.scss';
-
-const rootElem = document.querySelector('#root');
-
-ReactDOM.render(<ConnectionStatus />, rootElem);

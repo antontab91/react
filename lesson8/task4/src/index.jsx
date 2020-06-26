30ec4e39232e7592e702ad7a848d5bb0ddbdabea
Milkweedd <antontab91@gmail.com>
2020-06-26 23:25:38 +0300

    AS

--- lesson8/task4/src/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import User from './User';
-import './index.scss';
-
-const rootElem = document.querySelector('#root');
-
-ReactDOM.render(<User userId="SonyWWS" />, rootElem);

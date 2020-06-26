7b228f9d7f4c073e874a31eddb80cdb8ee515bdd
Milkweedd <antontab91@gmail.com>
2020-06-26 23:24:19 +0300

    SA

--- lesson8/task1/src/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Life from './Life';
-
-const rootElem = document.querySelector('#root');
-
-ReactDOM.render(<Life />, rootElem);

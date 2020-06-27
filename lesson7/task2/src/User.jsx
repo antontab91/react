b8382a40232c57dd1a000cfe6a54d9fc0b27c301
Milkweedd <antontab91@gmail.com>
2020-06-28 02:03:26 +0300

    as

--- lesson7/task2/src/User.jsx
+++ lesson7/task2/src/User.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
 
-const User = ({ name, age }) => {
+const User = ({ name, age }){
     return (
-        <li className='user'>
-            <span className='name'>{name}</span>
-            <span className='age'>{age}</span>
-        </li>
+
     )
 }
 

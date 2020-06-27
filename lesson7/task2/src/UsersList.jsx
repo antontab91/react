d92a3f83913c7a6e4c3296584ceb61ea9f385a34
Milkweedd <antontab91@gmail.com>
2020-06-27 03:57:39 +0300

    as

--- lesson7/task2/src/UsersList.jsx
+++ lesson7/task2/src/UsersList.jsx
@@ -36,6 +36,7 @@ class UsersList extends React.Component {
                     {
                         usersList.map((user) => {
                             return <User key={user.id} {...user} />
+
                         })
                     }
                 </ul>
@@ -45,47 +46,5 @@ class UsersList extends React.Component {
     }
 }
 
-
-
-// class UsersList extends Component {
-//     constructor(props) {
-//         super(props);
-//     }
-//     state = {
-//         sorting: null,
-//     };
-//     toggleSorting = () => {
-//         const newSorting = this.state.sorting === "asc" ? "desc" : "asc";
-//         this.setState({
-//             sorting: newSorting,
-//         });
-//     };
-
-//     render() {
-//         let usersList;
-//         if (this.state.sorting) {
-//             usersList = this.props.users
-//                 .slice()
-//                 .sort((a, b) =>
-//                     this.state.sorting === "asc" ? a.age - b.age : b.age - a.age
-//                 );
-//         } else {
-//             usersList = this.props.users;
-//         }
-
-//         return (
-//             <div>
-//                 <button className="btn" onClick={this.toggleSorting}>
-//                     {this.state.sorting || "-"}
-//                 </button>
-//                 <ul className="users">
-//                     {usersList.map((user) => (
-//                         <User key={user.id} {...user} />
-//                     ))}
-//                 </ul>
-//             </div>
-//         );
-//     }
-// }
 
 export default UsersList;

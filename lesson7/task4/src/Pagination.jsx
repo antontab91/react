73423c4148d9ae560b03bd70aac07f30e2ead531
Milkweedd <antontab91@gmail.com>
2020-06-30 19:44:24 +0300

    ds

--- lesson7/task4/src/Pagination.jsx
+++ lesson7/task4/src/Pagination.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const Pagination = ({ goPrev, goNext, currentPage, totalItems, itemsPerPage }) => {
-  const isPrevPageAvailable = '';
-  const isNextPageAvailable = '';
+  const isPrevPageAvailable = currentPage === 0;
+  const isNextPageAvailable = currentPage === Math.ceil(totalItems / itemsPerPage) - 1;
   return (
     <div className="pagination">ыффы</div>
   );

19b84278ee87284338ad64874689b9211b855b00
Milkweedd <antontab91@gmail.com>
2020-06-30 03:24:17 +0300

    as

--- lesson7/task4/src/UsersList.jsx
+++ lesson7/task4/src/UsersList.jsx
@@ -1,31 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import User from './User';
+import Pagination from './Pagination';
 
-
-class UsersList extends React.Component {
-
+class UsersList extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      currentPage: 0,
+      itemsPerPage: 3,
+    };
   }
 
+  goPrev = () => {
+    this.setState({
+      currentPage: this.state.currentPage - 1,
+    });
+  }
+
+  goNext = () => {
+    this.setState({
+      currentPage: this.state.currentPage + 1,
+    })
+  }
 
   render() {
+    const first = this.state.currentPage * this.state.itemsPerPage;
+    const last = first + this.state.itemsPerPage;
+    const usersList = this.props.users.slice(first, last);
     return (
       <div>
-        {/* <Pagination /> */}
+        <Pagination
+          goPrev={this.goPrev}
+          goNext={this.goNext}
+          currentPage={this.state.currentPage}
+          itemsPerPage={this.state.itemsPerPage}
+          totalItems={this.props.users.length}
+        />
         <ul className="users">
-          {
-            this.props.users.map((user) => {
-              return (
-                <User {...user} />
-              )
-            })
-          }
+          {usersList.map(user =>
+            <User key={user.id} {...user} />)}
         </ul>
       </div>
     );
   }
 }
-
 
 export default UsersList;

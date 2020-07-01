22e40454b87f75c716f7aac1616b266fda2b5edc
Milkweedd <antontab91@gmail.com>
2020-07-01 03:01:49 +0300

    sa

---
+++ lesson8/task1/src/Life.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Life extends React.Component {
+  constructor(props) {
+    super(props);
+
+    console.log('constructor: good place to create state')
+  }
+
+  render() {
+    return (
+
+    )
+  }
+}
+
+export default Life;

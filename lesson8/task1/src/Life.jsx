ad9867d47d7b1fa0a6b6a9ce205c58119fb07a13
Milkweedd <antontab91@gmail.com>
2020-07-01 03:42:57 +0300

    sa

--- lesson8/task1/src/Life.jsx
+++ lesson8/task1/src/Life.jsx
@@ -7,10 +7,26 @@ class Life extends React.Component {
   }
 
   componentDidMount() {
-    console.log("componentDidMount: API calls, subscriptions");
+    console.log("componentDidMount: API calls, subscriptions"); // подписка на события , работа с дом на прямую , запросы к базе и тд 
   }
 
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render'); // определить нужно ли перерисовывать компоненту 
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');  // некоторые обновления на основе новых props
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount(): cleanup before DOM related to component will be removed');  // очистка перед удалением DOM, связанного с компонентом 
+  }
+
+
+
+
   render() {
+    console.log('return React element to build DOM') // вернуть элемент React для рендера в  DOM
     return (
       <div className="s">55</div>
     )

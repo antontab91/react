481616821fd45bcb2ce34b07645be16f29446b9d
Milkweedd <antontab91@gmail.com>
2020-07-01 03:48:08 +0300

    as

--- lesson8/task1/src/Life.jsx
+++ lesson8/task1/src/Life.jsx
@@ -1,3 +1,42 @@
+// import React from 'react';
+
+// class Life extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     console.log('constructor: good place to create state');
+//   }
+
+//   componentDidMount() {
+//     console.log("componentDidMount: API calls, subscriptions"); // подписка на события , работа с дом на прямую , запросы к базе и тд 
+//   }
+
+//   shouldComponentUpdate(nextProps, nextState) {
+//     console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render'); // определить нужно ли перерисовывать компоненту 
+//   }
+
+//   componentDidUpdate(prevProps, prevState) {
+//     console.log(
+//       "componentDidUpdate(prevProps, prevState): some updates based on new props"
+//     );
+//   }
+//   componentWillUnmount() {
+//     console.log('componentWillUnmount(): cleanup before DOM related to component will be removed');  // очистка перед удалением DOM, связанного с компонентом 
+//   }
+
+
+
+
+//   render() {
+//     console.log('return React element to build DOM') // вернуть элемент React для рендера в  DOM
+//     return (
+//       <div className="s">55</div>
+//     )
+//   }
+// }
+
+// export default Life;
+
+
 import React from 'react';
 
 class Life extends React.Component {
@@ -7,29 +46,25 @@ class Life extends React.Component {
   }
 
   componentDidMount() {
-    console.log("componentDidMount: API calls, subscriptions"); // подписка на события , работа с дом на прямую , запросы к базе и тд 
+    console.log('componentDidMount: API calls, subscriptions');
   }
 
   shouldComponentUpdate(nextProps, nextState) {
-    console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render'); // определить нужно ли перерисовывать компоненту 
+    console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render');
+    return this.props !== nextProps;
   }
 
   componentDidUpdate(prevProps, prevState) {
-    console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');  // некоторые обновления на основе новых props
+    console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');
   }
 
   componentWillUnmount() {
-    console.log('componentWillUnmount(): cleanup before DOM related to component will be removed');  // очистка перед удалением DOM, связанного с компонентом 
+    console.log('componentWillUnmount(): cleanup before DOM related to component will be removed');
   }
 
-
-
-
   render() {
-    console.log('return React element to build DOM') // вернуть элемент React для рендера в  DOM
-    return (
-      <div className="s">55</div>
-    )
+    console.log('return React element to build DOM');
+    return true;
   }
 }
 

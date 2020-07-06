59a976854efe2bc0b45ace7380cb8fabbf7e42ed
Milkweedd <antontab91@gmail.com>
2020-07-07 00:26:07 +0300

    вы

--- lesson8/task4/src/User.jsx
+++ lesson8/task4/src/User.jsx
@@ -8,32 +8,37 @@ class User extends React.Component {
     }
   }
 
-  componentDidMount() {
+  componentDidMount() {                                            // когда мы хотим получить данные из сервера нужно это делать не до первого рендера , а уже в компонет дид маунт 
     fetch(`https://api.github.com/users/${this.props.userId}`)
       .then((response) => {
-        return response.json()
+        return response.json();
+      }).then((data) => {
+        return (
+          this.setState({
+            user: data,
+          })
+        )
       })
-      .then((data) => {
-        return this.setState({
-          user: data,
-        });
-      });
   }
 
   render() {
+
+    const { user } = this.state;  // сначала разобрал стейт 
+
     if (!this.state.user) {
       return null;
     }
+    const { avatar_url, location, name } = user; // потом разобрал юзера 
     return (
       <div className="user">
         <img
           className="user__avatar"
-          src={this.state.user.avatar_url}
+          src={avatar_url}
           alt="User Avatar"
         />
         <div className="user__info">
-          <span className="user__name">{this.state.user.name}</span>
-          <span className="user__location">{this.state.user.location}</span>
+          <span className="user__name">{name}</span>
+          <span className="user__location">{location}</span>
         </div>
       </div>
     )

2c2b606b2c708914eeafcf153ec12ce8b637f850
Milkweedd <antontab91@gmail.com>
2020-07-06 21:00:01 +0300

    ds

--- lesson8/task4/src/User.jsx
+++ lesson8/task4/src/User.jsx
@@ -3,14 +3,23 @@ import React from 'react';
 class User extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      name: null,
+      locatiom: null,
+      avatarUrl: null,
+    }
   }
 
   render() {
     return (
       <div className="user">
-        <img className="" src="" alt="" />
+        <img
+          className="user__avatar"
+          src=""
+          alt="User Avatar"
+        />
         <div className="user__info">
-          <span className="user__name" ></span>
+          <span className="user__name"></span>
           <span className="user__location"></span>
         </div>
       </div>

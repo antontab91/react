c55ebb18a188c5e67f589e5c0dcb4349b7fa024d
Milkweedd <antontab91@gmail.com>
2020-07-07 00:46:41 +0300

    ds

---
+++ lesson8/task5/src/ConnectionStatus.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class ConnectionStatus extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <div className="status status_offline"></div>
+    )
+  }
+}
+
+export default ConnectionStatus;

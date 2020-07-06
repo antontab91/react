2ec229d636be76ec32aa2bd2a70b34ef0fb7ae55
Milkweedd <antontab91@gmail.com>
2020-07-06 18:53:19 +0300

    axa

---
+++ lesson8/task4/src/User.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class User extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="cxs">dssd</div>
+    )
+  }
+}
+
+export default User;

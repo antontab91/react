1a105a20d3adac9b96781347fee168893c17c6e5
Milkweedd <antontab91@gmail.com>
2020-07-08 16:04:29 +0300

    ds

--- lesson9/task1/src/Search.jsx
+++ lesson9/task1/src/Search.jsx
@@ -16,13 +16,14 @@ class Search extends React.Component {
     })
   }
 
-  allertText = () => {
+  submit = (e) => {
+    e.preventDefault()
     alert(`Search text: ${this.state.value}`)
   }
 
   render() {
     return (
-      <form onSubmit={this.allertText} className="search" >
+      <form onSubmit={this.submit} className="search" >
         <input onChange={this.handleChange} value={this.state.value} type="text" className="search__input" />
         <button className="search__button">Search</button>
       </form>

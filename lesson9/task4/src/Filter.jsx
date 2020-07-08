ae158544ee80b6a485a3941ddcdec7cab8c1120b
Milkweedd <antontab91@gmail.com>
2020-07-09 01:25:36 +0300

    sa

--- lesson9/task4/src/Filter.jsx
+++ lesson9/task4/src/Filter.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Filter = (props) => {
   return (
     <div className="filter">
-      <span className="filter__count">5</span>
+      <span className="filter__count">{props.count}</span>
       <input onChange={props.handleChange} type="text" className="filter__input" value={props.value} />
     </div>
   )
--- lesson9/task4/src/UsersList.jsx
+++ lesson9/task4/src/UsersList.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Filter from './Filter.jsx'
+import User from './User.jsx'
 
 
 class UsersList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: ''
+      value: '',
     }
   }
 
@@ -19,11 +20,20 @@ class UsersList extends React.Component {
   }
 
   render() {
+
+    let usersList = this.props.users.filter((user) => {
+      return user.name.toLowerCase() === this.state.value.toLowerCase()
+    })
+
+    console.log(usersList)
+
     return (
       <div>
-        <Filter handleChange={this.handleChange} value={this.state.value} />
+        <Filter handleChange={this.handleChange} value={this.state.value} count={this.state.value.length} />
         <ul className="users">
-
+          {usersList.map((user) => {
+            return <User key={user.id} {...user} />
+          })}
         </ul>
       </div>
     )

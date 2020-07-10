23f39cdf19e3211f480819a8d188b4674afac9e0
Milkweedd <antontab91@gmail.com>
2020-07-10 16:17:20 +0300

    ві

--- lesson10/task1/src/App.jsx
+++ lesson10/task1/src/App.jsx
@@ -11,7 +11,7 @@ class App extends React.Component {
     return (
       <div className="page">
         <header className="header">
-          <UserMenu />
+          <UserMenu userId={`github`} />
         </header>
         <UserProfile userId={`github`} />
       </div>

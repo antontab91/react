64390dff05bc576c629b285b78bbaa74afa8ff06
Milkweedd <antontab91@gmail.com>
2020-07-13 00:56:58 +0300

    as

--- lesson10/task2/src/Profile.jsx
+++ lesson10/task2/src/Profile.jsx
@@ -2,18 +2,12 @@ import React from 'react';
 import UserForm from './UserForm.jsx'
 
 
-class Profile extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
-    return (
-      <div className="column">
-        <UserForm />
-      </div>
-    )
-  }
+const Profile = () => {
+  return (
+    <div className="column">
+      <UserForm />
+    </div>
+  )
 }
 
 export default Profile;

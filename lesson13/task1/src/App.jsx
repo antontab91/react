d6351ac3bf0887f74fda32db4fb93eb4dd2a9d11
Milkweedd <antontab91@gmail.com>
2020-07-27 01:46:45 +0300

    a

--- lesson13/task1/src/App.jsx
+++ lesson13/task1/src/App.jsx
@@ -1,18 +1,31 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from "./Home";
 import Products from "./Products";
 import Contacts from "./Contacts";
 import PageNotFound from "./PageNotFound";
 
 
+
 const App = () => {
   return (
     <div className="page">
-
-      <Home />
-      <Products />
-      <Contacts />
-      <PageNotFound />
+      <BrowserRouter>
+        <Switch>
+          <Route>
+            <Home exact path='/' />
+          </Route>
+          <Route>
+            <Products path='/Products' />
+          </Route>
+          <Route>
+            <Contacts path='/Contacts' />
+          </Route>
+          <Route>
+            <PageNotFound path='/PageNotFound' />
+          </Route>
+        </Switch>
+      </BrowserRouter>
 
     </div>
   )

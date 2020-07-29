a0a4d8f8f797ed1a01848b704e5223b6ee968bc7
Milkweedd <antontab91@gmail.com>
2020-07-30 02:30:16 +0300

    ds

--- lesson13/task3/src/User.jsx
+++ lesson13/task3/src/User.jsx
@@ -2,16 +2,43 @@ import React from "react";
 
 const url = `https://api.github.com/users`;
 
-const User = () => {
-  return (
-    <div className="user">
-      <img alt="User Avatar" src="https://avatars1.githubusercontent.com/u/9919?v=4" className="user__avatar" />
-      <div className="user__info">
-        <span className="user__name">GitHub</span>
-        <span className="user__location">San Francisco,CA</span>
+
+
+class User extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null
+    }
+  }
+
+  componentDidMount() {
+    this.fetchUser(this.props.match.params.userId);
+  }
+
+  fetchUser = () => {
+    fetch(`${url}/${userId}`)
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => {
+        return this.setState({
+          user: data
+        })
+      })
+  }
+
+  render() {
+    return (
+      <div className="user">
+        <img alt="User Avatar" src="https://avatars1.githubusercontent.com/u/9919?v=4" className="user__avatar" />
+        <div className="user__info">
+          <span className="user__name">GitHub</span>
+          <span className="user__location">San Francisco,CA</span>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default User;

818c8e6bb3d3ea4ace1e1cef221e4abb850d415d
Milkweedd <antontab91@gmail.com>
2020-07-30 02:41:32 +0300

    ds

--- lesson13/task3/src/User.jsx
+++ lesson13/task3/src/User.jsx
@@ -16,7 +16,15 @@ class User extends React.Component {
     this.fetchUser(this.props.match.params.userId);
   }
 
-  fetchUser = () => {
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      this.props.match.params.userId !== prevProps.match.params.userId
+    ) {
+      this.fetchUser(this.props.match.params.userId);
+    }
+  }
+
+  fetchUser = (userId) => {
     fetch(`${url}/${userId}`)
       .then((response) => {
         return response.json()
@@ -29,12 +37,18 @@ class User extends React.Component {
   }
 
   render() {
+    const { user } = this.state
+    if (!user) {
+      return null;
+    }
     return (
       <div className="user">
-        <img alt="User Avatar" src="https://avatars1.githubusercontent.com/u/9919?v=4" className="user__avatar" />
+        <img alt="User Avatar"
+          src={user.avatar_url}
+          className="user__avatar" />
         <div className="user__info">
-          <span className="user__name">GitHub</span>
-          <span className="user__location">San Francisco,CA</span>
+          <span className="user__name">{user.name}</span>
+          <span className="user__location">{user.location}</span>
         </div>
       </div>
     )

4cae1e17f457b125dea2868676da58b70cc96557
Milkweedd <antontab91@gmail.com>
2020-07-29 03:53:01 +0300

    ds

--- lesson13/task2/src/Product.jsx
+++ lesson13/task2/src/Product.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 const Product = ({ match }) => {
-  console.log(match)
+  // console.log(match)
   // return <div className="product">{match.params.productId}</div>;
-  return <div className="product">{match.params.productId} </div>;
+  return <div className="product">{`Product is a ${match.params.productId}`} </div>;
 };
 
 export default Product;
--- lesson13/task2/src/Products.jsx
+++ lesson13/task2/src/Products.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
 import Product from './Product';
 
-const Products = (...rest) => {
+const Products = ({ ...rest }) => {
   console.log(rest)
   return (
     <div className="page__content">

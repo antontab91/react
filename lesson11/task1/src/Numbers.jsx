c3b17da683ecabc50ea17a46ed75951de4c0dd30
Milkweedd <antontab91@gmail.com>
2020-07-31 03:34:17 +0300

    das

--- lesson11/task1/src/Numbers.jsx
+++ lesson11/task1/src/Numbers.jsx
@@ -2,9 +2,14 @@ import React from "react";
 
 class Numbers extends React.Component {
     shouldComponentUpdate(nextProps, nextState) {
-        console.log(nextProps.number);
-        console.log(this.props.number);
-        return this.props.number !== nextProps.number && this.props.title === nextProps.title;
+        // console.log(nextProps.number);
+        // console.log(this.props.number);
+        // return this.props.number !== nextProps.number && this.props.title === nextProps.title;
+
+        if (this.props.number === nextProps.number && this.props.title === nextProps.title) {
+            return false;
+        }
+        return true;
     }
 
 

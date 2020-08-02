cd45698584b618ffa95e8e976e4d0d3c774cf4ef
Milkweedd <antontab91@gmail.com>
2020-08-02 05:02:34 +0300

    assa

--- lesson11/task2/src/Dialog.jsx
+++ lesson11/task2/src/Dialog.jsx
@@ -1,24 +1,26 @@
 import React from "react";
-
-const Dialog = ({ title, isOpen, children, handleClose }) => {
-    if (!isOpen) {
-        return null;
-    }
+import PropTypes from "prop-types";
+const Dialog = ({ isOpen, children, title, handleClose }) => {
+    if (!isOpen) return null;
     return (
         <div className="dialog">
-            <div className="dialog__heading">
-                <h4 className="dialog__title">{title}</h4>
-                <button
-                    onClick={handleClose}
-                    className="dialog__close-btn">
-                    +
-                    </button>
-            </div>
-            <div className="dialog__content">
-                {children}
-            </div>
+            <div className="dialog__title">{title}</div>
+            <button className="dialog__close-btn" onClick={handleClose}>
+                +
+      </button>
+            <div className="dialog__content">{children}</div>
         </div>
-    )
-}
+    );
+};
+Dialog.propTypes = {
+    isOpen: PropTypes.bool,
+    children: PropTypes.element.isRequired,
+    title: PropTypes.string,
+    handleClose: PropTypes.func.isRequired,
+};
+Dialog.defaultProps = {
+    isOpen: false,
+    title: "",
+};
 
 export default Dialog;

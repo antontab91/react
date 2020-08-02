ed6d715a0dd1a1005c61bbfc7f3ed37748415d58
Milkweedd <antontab91@gmail.com>
2020-08-02 06:07:42 +0300

    фы

--- lesson13/task2/src/Product.jsx
+++ lesson13/task2/src/Product.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Product = ({ match }) => {
+const Product = ({ match }) => { // хистори , локейшен и меч, тут мы просто меч достали 
   return <div className="product">{match.params.productId}</div>;
 };
 

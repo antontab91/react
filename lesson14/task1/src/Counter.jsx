2dc288eec7cf2ff6da0c6c1717b1495447765921
Milkweedd <antontab91@gmail.com>
2020-08-04 03:51:08 +0300

    ds

--- lesson14/task1/src/Counter.jsx
+++ lesson14/task1/src/Counter.jsx
@@ -1,15 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 
-class Counter extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+// class Counter extends React.Component {
+//   constructor(props) {
+//     super(props);
 
-  render() {
-    return (
-      <div className="sd">dsds</div>
-    )
-  }
+//     this.state = {
+//       number: 0,
+//     }
+//   }
+
+//   increment = () => {
+//     this.setState({
+//       number: this.state.number + 1,
+//     })
+//   }
+
+//   decrement = () => {
+//     this.setState({
+//       number: this.state.number - 1,
+//     })
+//   }
+
+
+//   render() {
+//     return (
+//       <div className="counter">
+//         <button onClick={this.decrement} className="counter__button">
+//           -
+//         </button>
+
+//         <span className="counter__value">
+//           {this.state.number}
+//         </span>
+
+//         <button onClick={this.increment} className="counter__button">
+//           +
+//         </button>
+//       </div>
+//     )
+//   }
+// }
+
+const Counter = () => {
+  const [number, changeNumder] = useState(0);
+
+  return (
+    <div className="counter">
+      <button onClick={() => { changeNumder(number - 1) }} className="counter__button">
+        -
+        </button>
+
+      <span className="counter__value">
+        {number}
+      </span>
+
+      <button onClick={() => { changeNumder(number + 1) }} className="counter__button">
+        +
+        </button>
+    </div>
+  )
 }
 
 

6eaeffc963b56c8fb0722b614ee658c3bdc9e880
Milkweedd <antontab91@gmail.com>
2020-08-04 03:37:38 +0300

    sasa

---
+++ lesson14/task1/src/Counter.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="sd">dsds</div>
+    )
+  }
+}
+
+
+export default Counter;

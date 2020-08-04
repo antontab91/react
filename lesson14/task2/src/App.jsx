7ecb6624006afd8b9031f6c900206cace203065b
Milkweedd <antontab91@gmail.com>
2020-08-05 02:20:36 +0300

    ыф

--- lesson14/task2/src/App.jsx
+++ lesson14/task2/src/App.jsx
@@ -2,39 +2,33 @@ import React from 'react';
 import User from './User.jsx';
 import { BrowserRouter, Route, Link, Switch } from 'react-router-dom'
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+const App = () => {
+  return (
 
-    this.state = {
-      user: null
-    }
-
-  }
-
-  render() {
-    return (
-
-      <div className="page">
+    <div className="page">
+      <BrowserRouter>
         <div className="page__content">
           <h1>Users</h1>
-          <BrowserRouter>
-            <ul className="navigation">
-              <li className="navigation__item">
-                <a href="/users/github">Github</a>
-              </li>
-              <li className="navigation__item">
-                <a href="/users/facebook">Facebook</a>
-              </li>
-            </ul>
+          <ul className="navigation">
+            <li className="navigation__item">
 
-            <User />
-          </BrowserRouter>
+              <Link to="/users/github">Github</Link>
+            </li>
+            <li className="navigation__item">
 
+              <Link to="/users/facebook">Facebook</Link>
+            </li>
+          </ul>
+          <Switch>
+            <Route path="/users/:userId" component={User} ></Route>
+            <Route exact path="/users/" >
+              <span>Select a user please</span>
+            </Route>
+          </Switch>
         </div>
-      </div>
-    )
-  }
+      </BrowserRouter>
+    </div>
+  )
 }
 
 export default App;
--- lesson14/task2/src/User.jsx
+++ lesson14/task2/src/User.jsx
@@ -1,15 +1,61 @@
 import React from "react";
 
-const User = () => {
-  return (
-    <div className="user">
-      <img alt="User Avatar" src="https://avatars1.githubusercontent.com/u/9919?v=4" className="user__avatar" />
-      <div className="user__info">
-        <span className="user__name">GitHub</span>
-        <span className="user__location">San Francisco,CA</span>
+const url = `https://api.github.com/users`;
+
+
+class User extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: null,
+    }
+  }
+
+  componentDidMount() {
+    this.fetchUserData()
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    // console.log(prevProps.match.params.userId)  // те что были до этого 
+    // console.log(this.props.match.params.userId) // те которые мы сейчас передали 
+    if (this.props.match.params.userId !== prevProps.match.params.userId) {  //если те что мы сейчас передели не равны с теми что были до этого тогда перерендериваем  
+      this.fetchUserData()
+    }
+  }
+
+
+
+  fetchUserData() {
+    const user = this.props.match.params.userId;
+    // console.log(user);
+    fetch(`${url}/${user}`)
+      .then((responce) => {
+        return responce.json()
+      })
+      .then((user) => {
+        return this.setState({
+          user: user,
+        })
+      })
+  }
+
+  render() {
+    const { user } = this.state
+
+    if (!user) {
+      return null;
+    }
+    return (
+      <div className="user">
+        <img alt="User Avatar" src={user.avatar_url} className="user__avatar" />
+        <div className="user__info">
+          <span className="user__name">{user.name}</span>
+          <span className="user__location">{user.location}</span>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default User;

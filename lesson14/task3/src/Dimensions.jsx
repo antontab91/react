5ca5540213054f7b52cc0f81ab3f06be65b80a7c
Milkweedd <antontab91@gmail.com>
2020-08-05 05:26:39 +0300

    sa

--- lesson14/task3/src/Dimensions.jsx
+++ lesson14/task3/src/Dimensions.jsx
@@ -30,7 +30,7 @@ const Dimensions = () => {
 
 
   return (
-    <div className="dimensions">{`${dimensions.width} - ${dimensions.height}`}</div>
+    <div className="dimensions">{`${dimensions.width}px - ${dimensions.height}px`}</div>
   )
 }
 

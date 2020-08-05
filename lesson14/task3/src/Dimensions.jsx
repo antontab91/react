676b5d2aa63562b0ce70b82e5417a41a54f426f3
Milkweedd <antontab91@gmail.com>
2020-08-05 05:22:20 +0300

    ds

--- lesson14/task3/src/Dimensions.jsx
+++ lesson14/task3/src/Dimensions.jsx
@@ -1,41 +1,76 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-class Dimensions extends React.Component {
-  constructor(props) {
-    super(props);
+const Dimensions = () => {
+  const [dimensions, setDimensions] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  })
 
-    this.state = {
+
+  useEffect(() => {
+    setDimensions({
       width: window.innerWidth,
-      height: window.innerHeight,
+      height: window.innerHeight
+    })
+
+    const onResize = (e) => {
+      setDimensions({
+        width: e.target.innerWidth,
+        height: e.target.innerHeight,
+      })
     }
-  }
+    window.addEventListener('resize', onResize);
 
-  componentDidMount() {
-    window.addEventListener('resize', this.onResize)
-  }
+    return () => {
+      window.removeEventListener('resize', onResize);
+    }
 
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.onResize)
-  }
+  }, []);
 
-  onResize = (e) => {
-    const { innerWidth, innerHeight } = e.target;
-    this.setDimensions(innerWidth, innerHeight)
-  }
 
-  setDimensions = (width, height) => {
-    this.setState({
-      width,
-      height,
-    });
-  }
 
-  render() {
-    return (
-      <div className="dimensions">{`${this.state.width} - ${this.state.height}`}</div>
-    )
-  }
+  return (
+    <div className="dimensions">{`${dimensions.width} - ${dimensions.height}`}</div>
+  )
 }
+
+
+// class Dimensions extends React.Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       width: window.innerWidth,
+//       height: window.innerHeight,
+//     }
+//   }
+
+//   componentDidMount() {
+//     window.addEventListener('resize', this.onResize)
+//   }
+
+//   componentWillUnmount() {
+//     window.removeEventListener('resize', this.onResize)
+//   }
+
+//   onResize = (e) => {
+//     const { innerWidth, innerHeight } = e.target;
+//     this.setDimensions(innerWidth, innerHeight)
+//   }
+
+//   setDimensions = (width, height) => {
+//     this.setState({
+//       width,
+//       height,
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <div className="dimensions">{`${this.state.width} - ${this.state.height}`}</div>
+//     )
+//   }
+// }
 
 export default Dimensions;
 

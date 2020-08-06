327fd00c12b794e7bca238686c8faaa003e044e8
Milkweedd <antontab91@gmail.com>
2020-08-06 05:52:07 +0300

    sd

--- lesson14/task5/src/Clock.jsx
+++ lesson14/task5/src/Clock.jsx
@@ -10,60 +10,59 @@ const getTime = (timezone) => {
   return locationTimeZone;
 };
 
-class Clock extends React.Component {
-  constructor(props) {
-    super(props)
+// class Clock extends React.Component {
+//   constructor(props) {
+//     super(props)
 
-    this.state = {
-      cityTime: getTime(this.props.offset).toLocaleTimeString("en-US"),
-    }
-  }
+//     this.state = {
+//       cityTime: getTime(this.props.offset).toLocaleTimeString("en-US"),
+//     }
+//   }
 
-  componentDidMount() {
-    this.interval = setInterval(() => {
-      this.setState({
-        cityTime: getTime(this.props.offset).toLocaleTimeString("en-US")
-      })
-    }, 1000)
-  }
+//   componentDidMount() {
+//     this.interval = setInterval(() => {
+//       this.setState({
+//         cityTime: getTime(this.props.offset).toLocaleTimeString("en-US")
+//       })
+//     }, 1000)
+//   }
 
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
+//   componentWillUnmount() {
+//     clearInterval(this.interval);
+//   }
 
-  render() {
-    return (
-      <div className="clock">
-        <div className="clock__location">{this.props.location}</div>
-        <div className="clock__time">{this.state.cityTime}</div>
-      </div>
-    )
-  }
-}
-
-export default Clock;
-
-
-// const Clock = ({ location, offset }) => {
-//   const [cityTime, setTime] = useState(
-//     getTime(offset).toLocaleTimeString("en-US")
-//   );
-
-//   useEffect(() => {
-//     const timerId = setInterval(() => {
-//       setTime(getTime(offset).toLocaleTimeString("en-US"));
-//     }, 1000);
-//     return () => {
-//       clearInterval(timerId);
-//     };
-//   }, []);
-
-//   return (
-//     <div className="clock">
-//       <div className="clock__location">{location}</div>
-//       <div className="clock__time">{cityTime}</div>
-//     </div>
-//   );
-// };
+//   render() {
+//     return (
+//       <div className="clock">
+//         <div className="clock__location">{this.props.location}</div>
+//         <div className="clock__time">{this.state.cityTime}</div>
+//       </div>
+//     )
+//   }
+// }
 
 // export default Clock;
+
+
+const Clock = ({ location, offset }) => {
+  const [cityTime, setcityTime] = useState(getTime(offset).toLocaleTimeString("en-US"));
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setcityTime(getTime(offset).toLocaleTimeString("en-US"))
+    }, 1000);
+    return () => {
+      clearInterval(interval)
+    };
+  }, []);
+
+
+  return (
+    <div className="clock">
+      <div className="clock__location">{location}</div>
+      <div className="clock__time">{cityTime}</div>
+    </div>
+  );
+};
+
+export default Clock;

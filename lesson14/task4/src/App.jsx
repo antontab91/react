8677cf3e0c36a761d8a6e878c6c3b4bbd97c2d52
Milkweedd <antontab91@gmail.com>
2020-08-06 03:50:43 +0300

    ds

--- lesson14/task4/src/App.jsx
+++ lesson14/task4/src/App.jsx
@@ -1,40 +1,8 @@
-import React from 'react';
+import React from "react";
+import ConnectionStatus from "./ConnectionStatus";
 
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      status: "offline",
-    }
-  }
-
-  componentDidMount() {
-    window.addEventListener('online', this.updateStatus);
-    window.addEventListener("offline", this.updateStatus);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('online', this.updateStatus);
-    window.removeEventListener("offline", this.updateStatus);
-  }
-
-  updateStatus = (e) => {
-    this.setState({
-      status: e.type
-    })
-  }
-
-  updateClass = () => {
-    return `status ${this.state.status === "online" ? "status_online" : "status_offline"}`
-  }
-
-  render() {
-    return (
-      <div className={`${this.updateClass()}`}>{this.state.status}</div>
-    )
-  }
-}
+const App = () => {
+  return <ConnectionStatus />;
+};
 
 export default App;
---
+++ lesson14/task4/src/ConnectionStatus.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+class ConnectionStatus extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      status: 'online',
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('online', this.updateStatus);
+    window.addEventListener("offline", this.updateStatus);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('online', this.updateStatus);
+    window.removeEventListener("offline", this.updateStatus);
+  }
+
+  updateStatus = (event) => {
+    this.setState({
+      status: event.type,
+    });
+  }
+
+  setClassStatus = (status) => {
+    return `status ${status === "online" ? "status_online" : "status_offline"}`
+  }
+
+  render() {
+    return (
+      <div className={this.setClassStatus(this.state.status)}>{this.state.status}</div>
+    )
+  }
+}
+
+export default ConnectionStatus;
